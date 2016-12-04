6c049d6f104e5e21e2068487f750ecfaec9c22bc
Luiz <argentinaluiz@gmail.com>
2016-12-04 15:52:46 -0200

    Corrigindo erros

--- module/PsBase/src/Module.php
+++ module/PsBase/src/Module.php
@@ -12,14 +12,4 @@ class Module
     {
         return include __DIR__ . '/../config/module.config.php';
     }
-
-    public function getAutoloaderConfig () {
-        return array(
-            'Zend\Loader\StantardAutoLoader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                )
-            )
-        );
-    }
 }
